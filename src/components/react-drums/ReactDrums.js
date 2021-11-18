764aba2eaf2b72fb3a992ab02607ad6377aa516f
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-11-18 19:18:52 +0700

    updating codes

--- src/components/react-drums/ReactDrums.js
+++ src/components/react-drums/ReactDrums.js
@@ -1,86 +1,101 @@
 import React, {useState, useEffect} from "react";
 
-export default function ReactDrums() {
-
-  const [display, setDisplay] = useState('');
- 
-  const drums = [
-      {
-        keyCodes: 81, key: "Q", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/20[kb]80s-LOWCONGA.wav.mp3", name: "lowconga"
-      },
-      {
-        keyCodes: 87, key: "W", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/12[kb]80s-Bdrum1.wav.mp3", name: "bdrum"
-      },
-      {
-        keyCodes: 69, key: "E", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/18[kb]80s-COWBELL1.wav.mp3", name: "cowbell"
-      },
-      {
-        keyCodes: 65, key: "A", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/87[kb]80s-CRASH1.wav.mp3", name: "crash"
-      },
-      {
-        keyCodes: 83, key: "S", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/11[kb]80s-HHCLOSE1.wav.mp3", name: "hhclose"
-      },
-      {
-        keyCodes: 68, key: "D", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-SNARE1.wav.mp3", name: "snare"
-      },
-      {
-        keyCodes: 90, key: "Z", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-SNARE1.wav.mp3", name: "tom1"
-      },
-      {
-        keyCodes: 88, key: "X", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-TOM2.wav.mp3", name: "tom2"
-      },
-      {
-        keyCodes: 67, key: "C", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/9[kb]80s-HHCLOSE2.wav.mp3", name: "hhclose"
-      },
-    ]
+const drums = [
+  {
+    keyCodes: 81, key: "Q", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/20[kb]80s-LOWCONGA.wav.mp3", name: "lowconga"
+  },
+  {
+    keyCodes: 87, key: "W", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/12[kb]80s-Bdrum1.wav.mp3", name: "bdrum"
+  },
+  {
+    keyCodes: 69, key: "E", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/18[kb]80s-COWBELL1.wav.mp3", name: "cowbell"
+  },
+  {
+    keyCodes: 65, key: "A", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/87[kb]80s-CRASH1.wav.mp3", name: "crash"
+  },
+  {
+    keyCodes: 83, key: "S", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/11[kb]80s-HHCLOSE1.wav.mp3", name: "hhclose"
+  },
+  {
+    keyCodes: 68, key: "D", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-SNARE1.wav.mp3", name: "snare"
+  },
+  {
+    keyCodes: 90, key: "Z", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-SNARE1.wav.mp3", name: "tom1"
+  },
+  {
+    keyCodes: 88, key: "X", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/19[kb]80s-TOM2.wav.mp3", name: "tom2"
+  },
+  {
+    keyCodes: 67, key: "C", sound: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/9[kb]80s-HHCLOSE2.wav.mp3", name: "hhclose"
+  },
+]
   
-  const DrumMachine = (props) => {
-        
-    useEffect(() => {
-      document.addEventListener('keydown', handleKeyPress);      
-      return () => {
-        document.removeEventListener('keydown', handleKeyPress);
-      }
-    }, []);
-    
-    function handleKeyPress(e) {
-      console.log(e.keyCode);
-        if (e.keyCode === props.keyCodes) {
-          playAudio();          
-        }
-      }
- 
-    function playAudio() {
-      const audio = document.getElementById(props.soundId);
-      audio.play();
-    } 
-    
-    return (
-      <>
-        <button onClick={playAudio} className="btn btn-primary p-4 m-1 drum-pad w-100" id={props.name}>
-          {props.label}
-          <audio className="clip" id={props.soundId} src={props.sound} type="audio/mpeg" />
-        </button>
-      </>
-    );
-  }
-  
+const Display = ({ description }) => {
   return (
-  <>
-      <div className="container">
-        <h1 className="text-center my-5">React Drum</h1>
-        <div id="drum-machine">
-          <div id="display">{display}</div>
-            <div className="row row-cols-3 mx-auto" style={{maxWidth: '500px'}}>
-              { drums.map(drum => (
-                <div key={drum.key} className="col">
-                  <DrumMachine key1={drum.key} label={drum.key} soundId={drum.key} sound={drum.sound} name={drum.name} keyCodes={drum.keyCodes} nameDisplay={drum.name} />
-                </div>
-              ))}
-            </div>
-            <p className="text-center my-3">Play with your keyboard or click the button</p>
-        </div>
+    <div className="text-center">
+      <div id="display" className="display">
+        <h1>{description}</h1>
       </div>
-  </>
+    </div>
+  );
+};
+
+const DrumPad = ({ data, audio, display }) => {
+  return (
+    <div className="row row-cols-3 mx-auto" style={{width: '500px'}}>
+      {data.map(({ key, name, sound }) => (
+        <div className="col">
+        <div
+          id={name}
+          className="drum-pad col btn btn-primary p-4 m-1 w-100"
+          key={key}
+          onClick={() => {
+            audio(key);
+            display(name);
+          }}
+          >
+          {key}
+          <audio id={key} className="clip" src={sound}>
+            Your browser does not support the <code>audio</code> element.
+          </audio>
+        </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+  
+export default DrumMachine = () => {
+  const [display, setDisplay] = React.useState('play!');
+
+  React.useEffect(() => {
+    const handlekeydownEvent = (event) => {
+      const { keyCode } = event;
+      drums.forEach(item => {
+        if (item.keyCodes === keyCode) {
+          playAudio(item.key);
+          setDisplay(item.name);
+        }
+      });
+    };
+
+    document.addEventListener("keyup", handlekeydownEvent);
+    return () => {
+      document.removeEventListener("keyup", handlekeydownEvent);
+    }
+  }, []);
+
+  const playAudio = (name) => {
+    const audio = document.getElementById(name);
+    audio.play();
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div id="drum-machine" className="drum-machine">
+        <Display description={display} />
+        <DrumPad data={drums} audio={playAudio} display={setDisplay}/>
+      </div>
+    </div>
   );
 }
