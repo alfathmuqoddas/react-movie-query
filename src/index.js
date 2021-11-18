831d7f4cb6c81cd28f031a2b7918e758afed50c9
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-11-19 06:29:53 +0700

    delete drums page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import Login from "./components/firebase-app/Login"
 import Register from "./components/firebase-app/Register"
 import Profile from "./components/firebase-app/Profile"
 import EditProfile from "./components/firebase-app/EditProfile"
-import DrumMachine from "./components/react-drums/ReactDrums"
 
 const routs = (
    <Router>
@@ -26,7 +25,6 @@ const routs = (
          <Route exact path="/firebase-app/register" component={Register} />
          <Route exact path="/firebase-app/profile" component={Profile} />
          <Route exact path="/firebase-app/edit-profile" component={EditProfile} />
-         <Route exact path="/react-drums" component={DrumMachine} />
       </div>
     </Router>
  );
